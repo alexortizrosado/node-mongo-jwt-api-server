0a69505dbc5315629a0545f64ca37af010eb52ea
Alex Ortiz-Rosado <alex@breezechms.com>
2021-04-11 22:11:01 -0700

    Add packages and auth routes

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth.js");
+// const postsRoutes = require("./routes/posts.js");
+
+require("dotenv/config.js");
+
+const path = __dirname + "/views";
+const DB_URL = process.env.DB_URL;
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/user", authRoutes);
+// app.use("/posts", postsRoutes);
+
+app.get("/", (req, res) => {
+	res.sendFile(`${path}/index.html`);
+});
+
+async function connectToDB() {
+	try {
+		await mongoose.connect(DB_URL, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+		});
+		console.log("Connected to MongoDB");
+	} catch (err) {
+		console.log(`Error connecting to MongoDB: ${err}`);
+		process.exit(1);
+	}
+}
+
+connectToDB();
+
+app.listen(PORT, () => {
+	console.log(`Server running on port: http://localhost:${PORT}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,89 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User.js");
+
+router.post("/auth/register", async (req, res) => {
+	try {
+		const { error } = validate(req.body);
+		if (error) return res.status(400).send(error?.details[0].message);
+
+		const usernameExists = await User.findOne({
+			username: req.body.username,
+		});
+		if (usernameExists) return res.status(400).send("Username already exists");
+
+		const salt = await bcrypt.genSalt(10);
+		const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+		const user = new User({
+			username: req.body.username,
+			password: hashPassword,
+		});
+		const savedUser = await user.save();
+
+		const token = jwt.sign(
+			{
+				_id: savedUser._id,
+			},
+			process.env.JWT_SECRET
+		);
+
+		res.header("token", token).send({
+			data: {
+				token,
+			},
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(400).send(err);
+	}
+});
+
+router.post("/auth/login", async (req, res) => {
+	try {
+		const { error } = authValidation(req.body);
+		if (error) return res.status(400).send(error?.details[0].message);
+
+		const user = await User.findOne({
+			username: req.body.username,
+		});
+		if (!user) return res.status(400).send("Invalid username or password");
+
+		const validPassword = await bcrypt.compare(
+			req.body.password,
+			user.password
+		);
+		if (!validPassword)
+			return res.status(400).send("Invalid username or password");
+
+		const token = jwt.sign(
+			{
+				_id: user._id,
+			},
+			process.env.JWT_SECRET
+		);
+
+		res.header("token", token).send({
+			data: {
+				token,
+			},
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(400).send(err);
+	}
+});
+
+const validate = (body) => {
+  const validationSchema = Joi.object({
+		username: Joi.string().alphanum().min(3).max(30).required(),
+		password: Joi.string()
+			.pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+			.required(),
+	});
+	return validationSchema.validate(body);
+};
+
+module.exports = router;
